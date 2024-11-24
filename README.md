2f26a6f0f8b4bc01e1a8e52379fa5bb7b5991d64
Daniel Rodrigo Weiss <daniel.r.weiss13@gmail.com>
2024-11-24 03:29:22 -0300

    Comentários melhorados

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Imersao_backend_alura
-Projeto de API de um repositório de fotos desenvolvido durante a Imersão Dev Back-End com a Alura. {dependências: cors, dotenv, express, generative-ai, mongodb, multer.}
+Projeto de API de um repositório de fotos desenvolvido durante a Imersão Dev Back-End com a Alura.
+{dependências: cors, dotenv, express, generative-ai, mongodb, multer.}
--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -16,7 +16,7 @@ export async function listarPost(req, res) {
 };
 getPost
 
-//controlador da chamada da função criarPost sem enviar a imagem e retorna o resultado.
+//controlador da chamada da função criarPost sem enviar a imagem e retorna o resultado. (A)
 export async function novoPost(req, res) {
     const novo_post = req.body;
     console.log("Dados: ", novo_post);
@@ -29,7 +29,7 @@ export async function novoPost(req, res) {
     };
 };
 
-//controlador da chamada da função que insere imagem no banco de dados e renomeia de acordo com um id
+//controlador da chamada da função que insere imagem no banco de dados e renomeia de acordo com um id (B)
 export async function uploadImagem(req, res) {
     try {
         const id = req.params.id;
@@ -42,26 +42,7 @@ export async function uploadImagem(req, res) {
     };
 };
 
-//controlador da chamada da função criarPost com a imagem e retorna o resultado.
-export async function novoPostComImagem(req, res) {
-    try {
-        const imageBuffer = fs.readFileSync(req.file.path)
-        const descricao = await gerarDescricaoComGemini(imageBuffer);
-        const novo_post = {
-            descricao: descricao,
-            alt: descricao,
-        }
-        const post_criado = await criarPost(novo_post);
-        const imagem_atualizada = `uploads/${post_criado.insertedId}.png`;
-        fs.renameSync(req.file.path, imagem_atualizada);
-        res.status(200).json(post_criado);
-    } catch(erro) {
-        console.error(erro.message);
-        res.status(500).json({"Erro":"Falha na requisição."});
-    };
-};
-
-//controlador da chamada da função atualizaNovoPost dentro de um try e retorna o resultado.
+//controlador da chamada da função atualizaNovoPost que gera descrição da imagem por ia e retorna o resultado. (C)
 export async function atualizaNovoPost(req, res) {
     const id = req.params.id;
     const urlImagem = `http://localhost:3000/${id}.png`
@@ -75,6 +56,25 @@ export async function atualizaNovoPost(req, res) {
         }
         const post_atualizado = await atualizarPost(id, post);
         res.status(200).json(post_atualizado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição."});
+    };
+};
+
+//controlador da chamada da função criarPost com a imagem, e adiciona descrição por ia e retorna o resultado. (A + B + C)
+export async function novoPostComImagem(req, res) {
+    try {
+        const imageBuffer = fs.readFileSync(req.file.path)
+        const descricao = await gerarDescricaoComGemini(imageBuffer);
+        const novo_post = {
+            descricao: descricao,
+            alt: descricao,
+        }
+        const post_criado = await criarPost(novo_post);
+        const imagem_atualizada = `uploads/${post_criado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagem_atualizada);
+        res.status(200).json(post_criado);
     } catch(erro) {
         console.error(erro.message);
         res.status(500).json({"Erro":"Falha na requisição."});
